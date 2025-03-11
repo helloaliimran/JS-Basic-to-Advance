755f489dc2ae54bead1d6558fb845507c4f4eac1
helloaliimran <hellolaiimran@yahoo.com>
2025-03-11 11:51:43 +0500

    even number check again.

--- raw.js
+++ raw.js
@@ -11,3 +11,21 @@ a.forEach(element => {
 
 var b=  [1,1,2,2,3,4];
 var c=  [5,6,7,8,9,41];
+
+
+
+b.forEach(element => {
+
+    console.log(element);
+    if(element%2==0)console.log('even');
+    else console.log('not even');
+
+});
+
+c.forEach(element => {
+
+    console.log(element);
+    if(element%2==0)console.log('even');
+    else console.log('not even');
+
+});
