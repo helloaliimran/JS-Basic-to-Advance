46cb34af857b7d6abbf0c0538b2feffbf5aacb47
helloaliimran <hellolaiimran@yahoo.com>
2025-03-11 11:22:20 +0500

    test commit

--- raw.js
+++ raw.js
@@ -1,5 +1,12 @@
 //array declaration
 var a = [1,1,22,2,3,4,5];
 a.forEach(element => {
+
     console.log(element);
+    if(element%2==0)console.log('even');
+    else console.log('not even');
+
 });
+
+
+var b=  [1,1,2,2,3,4];
