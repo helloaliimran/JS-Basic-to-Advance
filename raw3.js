81f59bc0cef6efe8c23466a259ca0ad3b2840c1a
helloaliimran <hellolaiimran@yahoo.com>
2025-03-19 08:19:28 +0500

    promise example

--- raw3.js
+++ raw3.js
@@ -7,3 +7,19 @@ console.log(1==="1")
 console.log(1=="1")
 console.log(null==undefined)
 console.log(null===undefined)
+
+const pizzaOrder = new Promise((resolve,reject)=>{
+let isPizzaReady = true;
+if (isPizzaReady){
+    resolve("Pizza is ready");
+}else{
+    reject("Sorry No pizza today.");
+}
+});
+
+pizzaOrder.then((mess)=>{
+console.log(mess);
+})
+.catch((mess)=>{
+    console.log(mess);
+})
